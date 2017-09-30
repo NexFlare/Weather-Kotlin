3cff88829f8cc14c8041df672c1043511dbd9f83
NexFlare <NexFlare@users.noreply.github.com>
2017-09-30 21:10:17 +0530

    Gps dialog added

--- app/src/main/java/com/nexflare/weatherapp/LocationUtil.kt
+++ app/src/main/java/com/nexflare/weatherapp/LocationUtil.kt
@@ -1,41 +1,89 @@
 package com.nexflare.weatherapp
 
-import android.content.Context
+import android.app.Activity
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.widget.Toast
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.location.*
 
 /**
  * Created by nexflare on 30/09/17.
  */
 
-class LocationUtil(var context: Context, val locationChangedListener:LocationChangedListener ) : GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+class LocationUtil(var context: Activity, val locationChangedListener: LocationChangedListener) : GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private var googleApiClient:GoogleApiClient
-    private var locationRequest:LocationRequest
+    var googleApiClient: GoogleApiClient
+    private var locationRequest: LocationRequest
+    private var REQUEST_CHECK_SETTINGS=0x1
 
     init {
-        googleApiClient=GoogleApiClient.Builder(context)
+        googleApiClient = GoogleApiClient.Builder(context)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build()
         googleApiClient.connect()
-        locationRequest= LocationRequest.create().setInterval(5000)
+        locationRequest = LocationRequest.create().setInterval(5000)
                 .setFastestInterval(3000)
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        checkSettings()
+        checkLocationSettings()
     }
 
-    private fun checkSettings() {
-        Toast.makeText(context,"Check the app",Toast.LENGTH_SHORT).show()
+
+    private fun checkLocationSettings() {
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+        builder.setAlwaysShow(true) //this is the key ingredient
+
+        val result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build())
+        result.setResultCallback(object : ResultCallback<LocationSettingsResult> {
+            override fun onResult(result: LocationSettingsResult) {
+                val status = result.status
+                when (status.statusCode) {
+                    LocationSettingsStatusCodes.SUCCESS -> if (!googleApiClient.isConnected) {
+                        googleApiClient.connect()
+                    } else {
+                        getLocation()
+                    }
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(context, REQUEST_CHECK_SETTINGS)
+                        } catch (e: IntentSender.SendIntentException) {
+                            // Ignore the error.
+                        }
+
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    }
+                }// All location settings are satisfied. The client can initialize location
+                // Location settings are not satisfied. However, we have no way to fix the
+                // settings so we won't show the dialog.
+            }
+        })
+
+    }
+
+    fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest) {
+            location ->
+            locationChangedListener.onLocationChanged(location.latitude, location.longitude)
+        }
+
     }
 
     override fun onConnected(p0: Bundle?) {
-        locationChangedListener.onLocationChanged(27.32,45.11)
+        getLocation()
     }
 
     override fun onConnectionSuspended(p0: Int) {
@@ -43,7 +91,7 @@ class LocationUtil(var context: Context, val locationChangedListener:LocationCha
     }
 
     override fun onConnectionFailed(p0: ConnectionResult) {
-        Toast.makeText(context,"Something went wrong",Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/nexflare/weatherapp/MainActivity.kt
+++ app/src/main/java/com/nexflare/weatherapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.nexflare.weatherapp
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_LOCATION:Int=2309
+    private val REQUEST_LOCATION: Int = 2309
+    private var REQUEST_CHECK_SETTINGS=0x1
+    lateinit var locationUtil: LocationUtil
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,37 +24,50 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     fun checkForPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                     && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_LOCATION)
-            }
-            else
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
+            } else
                 getLocation()
         }
     }
 
-    fun getLocation(){
-        val locationUtil=LocationUtil(this@MainActivity,object:LocationChangedListener{
+    fun getLocation() {
+        locationUtil = LocationUtil(this@MainActivity, object : LocationChangedListener {
             override fun onLocationChanged(latitute: Double?, longitude: Double?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(this@MainActivity,""+latitute+" "+longitude,Toast.LENGTH_SHORT).show()
+                locationUtil.googleApiClient.disconnect()
             }
-
-
         }
 
         )
 
     }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            REQUEST_LOCATION->{
-                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        when (requestCode) {
+            REQUEST_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getLocation()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            REQUEST_CHECK_SETTINGS->{
+                when(resultCode){
+                    Activity.RESULT_CANCELED->finish()
+
+                    else->if(locationUtil.googleApiClient.isConnected){
+                        locationUtil.getLocation()
+                    }
                 }
             }
         }
