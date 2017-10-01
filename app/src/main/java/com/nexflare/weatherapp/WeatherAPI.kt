af160906a654c5084d605dd1717a6409aac8fa8f
NexFlare <NexFlare@users.noreply.github.com>
2017-10-01 18:44:07 +0530

    Weather Model added

--- app/src/main/java/com/nexflare/weatherapp/WeatherAPI.kt
+++ app/src/main/java/com/nexflare/weatherapp/WeatherAPI.kt
@@ -1,7 +1,12 @@
 package com.nexflare.weatherapp
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 /**
  * Created by nexflare on 01/10/17.
  */
 interface WeatherAPI {
+    @GET("{location}")
+    fun getWeatherResponse(location:String): Call<WeatherResponse>
 }
